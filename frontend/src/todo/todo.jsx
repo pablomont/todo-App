6a3bd282583e9d52e8c60aea0eec88cf372acab3
Pablo Monteiro <paabloms@live.com>
2018-08-07 02:41:22 -0300

    added grid,if,iconButton

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
-import TodoList from './todoList';
+import TodoList from './todoList'
 
 
 export default class Todo extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {description: '', list: []}
+    }
+
+    handleAdd(){
+        console.log(this.state.description)
+    }
+
+    handleChange(e){
+        this.setState({description: e.target.value})
+    }
+   
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm description = {this.state.description} 
+                    handleAdd={() => this.handleAdd()}
+                    handleChange={(e) => this.handleChange(e)}
+                    />
                 <TodoList />
             </div>
         )

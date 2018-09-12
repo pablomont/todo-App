9a686851f8d825b6ebe3d5d497ce8dfd86d2c39d
Pablo Monteiro <paabloms@live.com>
2018-09-12 00:46:22 -0300

    added midlewares

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,21 +1,6 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
+    description: '',
     list:[
-        {
-            _id: 1,
-            description: 'Pagar fatura do cartão',
-            done: true
-        },
-        {
-            _id: 2,
-            description: 'Reunião com a equipe às 10:00',
-            done: false
-        },
-        {
-            _id: 3,
-            description: 'Consulta médicas na terça depois do alomoço',
-            done: false
-        },
     ]
 }
 
